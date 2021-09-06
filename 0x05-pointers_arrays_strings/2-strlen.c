0e408f015c5ca81107f9edb68b0c89f47146b5a1
NantaelM77 <natnaelmehari77@gmail.com>
2021-09-06 08:17:02 -0700

    Edti: 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - return string length
+ * @s: address of a variable
+ * Return: string length
+*/
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
