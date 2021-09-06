61d86d9938ff0570b0ca360caae53a85f5a1db60
NantaelM77 <natnaelmehari77@gmail.com>
2021-09-06 08:06:27 -0700

    Edit: 1-swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * swap_int - swap to numbers
+ * @a: address of a variable
+ * @b: address of a variable
+*/
+void swap_int(int *a, int *b)
+{
+	int p = *a;
+	*a = *b;
+	*b = p;
+}
