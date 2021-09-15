6493a09f4d7b5daa43490e6fcad35a8ed80e7c93
NantaelM77 <natnaelmehari77@gmail.com>
2021-09-15 10:14:52 -0700

    0x0-recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,13 +4,13 @@
  * power - return power using recursion
  * @base: base number
  * @a: power number
- * Return: 
+ * Return: return power
  */
 
 
-int power(int base, int a) {
-    if (a != 0)
-        return (base * power(base, a - 1));
-    else if (a < 0)
+int _pow_recursion(int x, int y) {
+    if (y != 0)
+        return (x * _pow_recursion(x, y - 1));
+    else if (y < 0)
         return -1;
 }
