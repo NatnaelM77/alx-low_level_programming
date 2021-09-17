b6bb7f98d218e98feab3293a15f656618da7ca7a
NantaelM77 <natnaelmehari77@gmail.com>
2021-09-17 04:06:44 -0700

    Add: args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* main - print out the number of command line arguments
+* @argc: the size of argv
+* @argv: is an array containing command line arguments
+* Return: 0
+*/
+
+int main(int argc, char const *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
