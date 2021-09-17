16d33e49ac5c2c5f18537ede4c35ef01bd6fe8c0
NantaelM77 <natnaelmehari77@gmail.com>
2021-09-17 04:13:58 -0700

    Add: 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+
+/**
+* main - print out all arguments
+* @argc: the size of argv
+* @argv: is an array containing command line arguments
+* Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	for (int i = 0; i < argc; ++i)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
