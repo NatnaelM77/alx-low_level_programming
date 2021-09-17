3ac5268d0845e8d47ca33c19873f0f563544c3d3
NantaelM77 <natnaelmehari77@gmail.com>
2021-09-17 03:45:06 -0700

    Add: 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print out name
+* @argc: the size of argv
+* @argv: is an array containing command ling arguments
+* Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
