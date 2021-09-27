66aabf70f1f7f4d528555bad8d0e796ac03bf56f
NantaelM77 <natnaelmehari77@gmail.com>
2021-09-27 10:09:38 -0700

    Create: dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - define dog
+ *@name: dog name
+ *@owner: dog owner
+ *@age: dog age
+ *
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
