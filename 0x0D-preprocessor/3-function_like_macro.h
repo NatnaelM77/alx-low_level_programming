b969f630625af5ddae75c11f2a24481f72ef2ff5
NantaelM77 <natnaelmehari77@gmail.com>
2021-09-28 05:46:36 -0700

    Add: main.h, Edit: 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,9 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
+#ifndef ABS_H
+#define ABS_H
 
-#define ABS(x) ((x) < 0 ? -(x) : (x))
+#include <math.h>
+
+#define ABS(x) (abs(x))
 typedef int make_iso_compilers_happy;
 
-#endif`
+#endif
