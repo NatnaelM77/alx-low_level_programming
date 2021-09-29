257aca002c5d5af066c948457d0b1cf901dcfce5
NatnaelM77 <natnaelmehari77@gmail.com>
2021-09-29 14:44:42 +0300

    Create 2-int_main.c

---
+++ 0x0F-function_pointers/2-int_main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - 
+ * @array: array
+ * @size: size of an array
+ * @action: callback function to check array elem
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+     
+}
