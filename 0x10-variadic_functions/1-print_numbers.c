eb6f131e9d75ff6e9484edfb56feffb9e773248e
NatnaelM77 <natnaelmehari77@gmail.com>
2021-09-30 19:48:57 +0300

    Create 1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - returns the sum of all its parameters.
+ * @separator: separator between numbers
+ * @n: size of number
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int num;
+	va_list valist;
+
+	va_start(valist, n);
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(valist, int);
+		printf("%d", num);
+		if (i < n - 1 && separator)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(valist);
+}
