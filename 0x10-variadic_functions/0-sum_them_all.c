17314786f7a0dddc23e30cfbf6e857d4b5c67964
NatnaelM77 <natnaelmehari77@gmail.com>
2021-09-30 18:22:37 +0300

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,8 +1,8 @@
-#include <variadic_functions.h>
+#include "variadic_functions.h"
 
 int sum_them_all(const unsigned int n, ...)
 {
-    int i;
+    unsigned int i;
     int sum = 0;
 
     va_list num;
