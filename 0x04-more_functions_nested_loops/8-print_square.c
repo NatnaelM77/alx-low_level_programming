901097f905bcf0fed1a032c8ad1b6438ed5c59a2
NantaelM77 <natnaelmehari77@gmail.com>
2021-10-02 02:42:18 -0700

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,14 +9,10 @@ void print_square(int size)
 {
 	int i, j;
 
-	if (size < 0)
-	{
-		_putchar('\n');
-	}
-	else 
+	if (size > 0)
 	{
 		for (i = 0; i < size; ++i)
-		{	
+		{
 			for (j = 0; j < size; ++j)
 			{
 				_putchar('#');
@@ -24,4 +20,6 @@ void print_square(int size)
 			_putchar('\n');
 		}
 	}
+	else
+		_putchar('\n');
 }
