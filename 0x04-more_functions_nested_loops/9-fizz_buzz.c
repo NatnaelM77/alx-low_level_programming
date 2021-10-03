665a828ff5adc524802ce1a85682c4d87f4ad2ec
NantaelM77 <natnaelmehari77@gmail.com>
2021-10-03 06:31:56 -0700

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 
 	int i;
 
-	for (i = 1; i <= 100; ++i)
+	for (i = 1; i <= 99; ++i)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
@@ -22,7 +22,7 @@ int main(void)
 		else
 			printf("%d ", i);
 	}
-	printf("\n");
+	printf("Buzz\n");
 
 	return (0);
 }
