095eb752edb995be37cfa6a703eb932d64077d64
NantaelM77 <natnaelmehari77@gmail.com>
2021-10-07 11:24:17 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,17 +8,19 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
-	
+	size_t s = 0;
+
 	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
+
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		count++;
+
+		s++;
 		h = h->next;
 	}
 
-	return (count);
+	return (s);
 }
