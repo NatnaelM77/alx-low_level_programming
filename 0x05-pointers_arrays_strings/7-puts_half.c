3b6f0d9efd48021edcc128bd2532140e4ae15a66
NantaelM77 <natnaelmehari77@gmail.com>
2021-10-08 05:21:55 -0700

    update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -32,19 +32,9 @@ void puts_half(char *str)
 	int i, n, len;
 
 	len = _strlen(str);
-
-	if (len % 2 != 0)
-	{
-		n = (len - 1) / 2;
-	}
-
-	if (len % 2 == 0)
-	{
-		n = len / 2;
-	}
+	n = (len % 2 == 0) ? len / 2 : (len - 1) / 2 + 1;
 
 	for (i = n; i < len; i++)
 		_putchar(*(str + i));
-
 	_putchar('\n');
 }
