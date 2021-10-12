47bfa4ff087039d2ae098a48c1235db63e3858ac
NatnaelM77 <natnaelmehari77@gmail.com>
2021-10-12 20:45:04 +0300

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,15 +6,15 @@
  *
  * Return: number of elements in the list
  */
-size_t print_listint(const listint_t *h)
-{
-	size_t count = 0;
+ size_t print_listint(const listint_t *h)
+ {
+ 	register size_t count = 0;
 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		count++;
-	}
-	return (count);
-}
+ 	while (h)
+ 	{
+ 		printf("%i\n", h->n);
+ 		h = h->next;
+ 		count++;
+ 	}
+ 	return (count);
+ }
