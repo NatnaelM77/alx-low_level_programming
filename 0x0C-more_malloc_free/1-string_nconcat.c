fa72064e786fca92d6bdadcf3d664a8a851aebde
natnaelm77 <natnaelmehari77@gmail.com>
2021-11-21 12:42:29 +0300

    fix betty-style error

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -44,7 +44,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (!ptr)
 		return (NULL);
 
-	for (i = 0; i < len; i++) {
+	for (i = 0; i < len; i++)
+	{
 		if (i < _strlen(s1))
 			ptr[i] = s1[i];
 		else
