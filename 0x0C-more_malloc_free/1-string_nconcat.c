1de1d45d4eae36225ef9be2ed80698c96b927199
NantaelM77 <natnaelmehari77@gmail.com>
2021-11-23 04:02:35 -0800

    Updat 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -37,11 +37,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 	if (!s2)
 		src = "";
 
-	ptr = malloc(strlen(dest) + n + 1);
+	ptr = malloc(_strlen(dest) + n + 1);
 	if (!ptr)
 		return (NULL);
 
-	for (i = 0; i < strlen(s1); i++)
+	for (i = 0; i < _strlen(s1); i++)
 		*(ptr + i) = *(s1 + i);
 
 	j = i;
