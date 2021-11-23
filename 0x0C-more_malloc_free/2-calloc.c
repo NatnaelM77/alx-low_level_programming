ac4ecf30d1b892cf7c57588f250e0af4c1e5189f
NantaelM77 <natnaelmehari77@gmail.com>
2021-11-23 06:44:08 -0800

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,13 +16,13 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
-	{
-		ptr = malloc(size);
+	ptr = malloc(nmemb * size);
 
-		if (!ptr)
-			return (NULL);
-	}
+	if (!ptr)
+		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		ptr[i] = '\0';
 
 	return (ptr);
 }
