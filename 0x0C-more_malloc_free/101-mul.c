3fe0f59f2307f48e38be02378a695639ddb14ccd
NantaelM77 <natnaelmehari77@gmail.com>
2021-11-23 06:10:11 -0800

    Add 101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * main - multiply two number
+ * @argc: argument length
+ * @argv: argument value
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int index = 1, result = 1;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	while (index < argc)
+	{
+		if (!atoi(argv[index]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+
+		result *= atoi(argv[index]);
+		index++;
+	}
+
+	printf("%d\n", result);
+
+	return (0);
+}
