29573d4cc2a0a5a636d674c9e7d429535ed98f82
Natnael <natnaelmehari77@gmail.com>
2021-11-29 21:16:34 +0300

    c dynamic libraries

---
+++ 0x18-dynamic_libraries/lib/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
+int div(int, int);
+int mod(int, int);
+
+#endif
---
+++ 0x18-dynamic_libraries/lib/mod.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mod - return remainder
+ * @a: first operand
+ * @b: second Operand
+ *
+ * Return: remainder of a, b
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
