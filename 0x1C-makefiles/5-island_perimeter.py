66828359dc973a1acd94a8c00b21b97d0af365ad
natnaelm77 <natnaelmehari77@gmail.com>
2022-02-12 15:33:15 +0300

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,9 +7,9 @@ Moudle island_perimeter
 def island_perimeter(grid):
 	"""returns the perimeter of the island described in grid."""
 	perimeter = 0
-	if len(grid) <= 100:
+	if len(grid) < 100:
 		for i in range(len(grid)):
-			if len(grid[i]) <= 100:
+			if len(grid[i]) < 100:
 				for j in range(len(grid[i])):
 					if grid[i][j] != 0:
 						if grid[i - 1][j] == 0:
