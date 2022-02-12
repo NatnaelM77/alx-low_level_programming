af96353ac6ba856794d700843aaf90cd9f9d6123
natnaelm77 <natnaelmehari77@gmail.com>
2022-02-12 15:28:41 +0300

    0x1C-makefiles

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+Moudle island_perimeter
+"""
+
+def island_perimeter(grid):
+	"""returns the perimeter of the island described in grid."""
+	perimeter = 0
+	if len(grid) <= 100:
+		for i in range(len(grid)):
+			if len(grid[i]) <= 100:
+				for j in range(len(grid[i])):
+					if grid[i][j] != 0:
+						if grid[i - 1][j] == 0:
+							perimeter += 1
+						if grid[i + 1][j] == 0:
+							perimeter += 1
+						if grid[i][j - 1] == 0:
+							perimeter += 1
+						if grid[i][j + 1] == 0:
+							perimeter += 1			
+	return perimeter
