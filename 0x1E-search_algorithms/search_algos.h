3aa17e9b2fb06c5a601d64d0b8684701a2961481
natnaelm77 <natnaelmehari77@gmail.com>
2022-03-28 22:00:28 +0300

    0x1E-search_algorithms

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,15 @@
+#ifndef SERACH_ALGOS
+#define SERACH_ALGOS
+
+#include <stdio.h>
+#include <stddef.h>
+
+/* Print Array */
+void print_array(int, int);
+
+/* Linear Search Algorithm */
+int linear_search(int *, size_t, int);
+/* Binary Search Algorithm */
+int binary_search(int *, size_t, int);	
+
+#endif /* SERACH_ALGOS */
