79879d48defb7fa4d8a5b56126e32a65d6d7027a
natnaelm77 <natnaelmehari77@gmail.com>
2022-03-31 01:15:34 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,19 +10,57 @@
 
 void print_times_table(int n)
 {
-	int i;
 	int j = 0;
+	int i, result;
 
-	if (n > 0 && n < 15)
+	if (n >= 0 && n < 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			for (;j < n; j++)
+			for (; j < n; j++)
 			{
-				printf("%d,   ", i * j);
+				result = i * j;
+				print_table(result, j, 0);
 			}
-			printf("%d\n", i * j);
+			print_table(i * j, j, 1);
 			j = 0;
+		}
+	}
+}
+
+/**
+ * print_table - print formated table
+ *
+ * @value: value to be printed
+ * @status: status value for newline
+ */
+
+void print_table(int value, int iter, int status)
+{
+	char * space = "   ";
+
+	if (iter == 0)
+	{
+		printf("%d,", value);
+	}
+	else
+	{
+		if (value > 9 && value < 100)
+		{
+			space = "  ";
+		}
+		else if (value > 99)
+		{
+			space = " ";
+		}
+
+		if (status == 1)
+		{
+			printf("%s%d\n", space, value);
+		}
+		else
+		{
+			printf("%s%d,", space, value);
 		}
 	}
 }
