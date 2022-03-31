af2fef6f303f62cde86f3d4f042068e0738b7ac2
natnaelm77 <natnaelmehari77@gmail.com>
2022-03-31 14:05:07 +0300

    Create 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_multiples - print multiples of 3
+ * and 5 less than 1024
+ *
+ * @n: max number
+ */
+
+void print_multiples(int n)
+{
+	int i = 1, total = 0;
+
+	for (; i < n; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			total += i;
+		}
+	}
+	printf("%d ", total);
+}
+
+/**
+ * main - Entry Point
+ * 
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	print_multiples(1024);
+
+	return (0);
+}
