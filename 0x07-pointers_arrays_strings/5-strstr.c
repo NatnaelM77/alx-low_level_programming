127ffa80adddecbb7f617dac9815765021c9c002
natnaelm77 <natnaelmehari77@gmail.com>
2022-03-31 19:18:38 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,20 +15,18 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; i < strlen(haystack); i++)
 	{
-		for (j = 0; j < strlen(needle); j++)
+		if (haystack[i] == needle[0])
 		{
-			if (haystack[i] == needle[j])
+			size_t len = (strlen(haystack) - i) + 1;
+			char *str = malloc(len);
+
+			for (j = 0; j < len; j++)
 			{
-				size_t len = (strlen(haystack) - i) + 1;
-				char *str = malloc(len);
-
-				for (j = 0; j < len; j++)
-				{
-					*(str + j) = *(haystack + i++);
-				}
-
-				return (str);
+				*(str + j) = *(haystack + i++);
 			}
+			*(str + j) = '\0';
+
+			return (str);
 		}
 	}
 	return (NULL);
