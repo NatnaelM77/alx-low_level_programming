bdbd908eb814ae13ec1c54a2fcdb5b69b5f0270d
natnaelm77 <natnaelmehari77@gmail.com>
2022-04-02 13:58:43 +0300

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * _memcpy - copy memory data from src to dest
+ * @dest: memory destination
+ * @src: memory source
+ * @n: size of new memory
+ */
+
+void _memcpy(void *dest, void *src, size_t n)
+{
+	size_t i;
+	char *csrc = (char *)src;
+	char *cdest = (char *)dest;
+
+	for (i = 0; i < n; i++)
+		cdest[i] = csrc[i];
+}
+
+/**
  * _realloc - reallocates a memory block using malloc and free
  *
  * @ptr: a pointer to the memory previously allocated
@@ -30,13 +47,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		size_t i;
 		void *new = malloc(new_size);
+		
 		if (!new)
 			return (NULL);
+		else
+			_memcpy(new, ptr, old_size);
 
-		for (i = 0; i < old_size; i++)
-		{
-			((char *)new)[i] = ((char *)ptr)[i];
-		}
 		free(ptr);
 		return (new);
 	}
