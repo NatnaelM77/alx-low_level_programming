03d97c577987c55c658071f1e263478a54c70d4e
NatnaelM77 <natnaelmehari77@gmail.com>
2022-04-28 18:45:39 +0300

    Jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -11,36 +11,35 @@
  * Return: the first index of where value is located or -1 if it fails
  */
 
-int jump_search(int *array, size_t size, int value) {
-	size_t i = 0;
-	size_t min = 0;
-	size_t block_size = (size_t) sqrt(size);
-	size_t max = block_size;
+int jump_search(int *array, size_t size, int value)
+{
+	int min = 0;
+	int block_size = (int) sqrt(size);
+	int max = block_size;
 
 	if (!array)
 		return (-1);
 
-	while (*(array + max) > value || max <= size) {
-		printf("Value checked array[%zu] = [%zu]\n", min, min);
+	while (*(array + max) > value || max < size)
+	{
+		printf("Value checked array[%d] = [%d]\n", min, min);
 		if (*(array + max) < value)
 		{
 			min = max;
 			max = max + block_size;
-		}
-		else
+		} else
 		{
 			break;
 		}
 	}
 
-	printf("Value found between indexes[%zu] = [%zu]\n", min, max);
-
-	for (; min <= max; min++) {
-		printf("Value checked array[%zu] = [%d]\n", min, *(array +
-			min));
-		if (value == *(array + min)) {
+	printf("Value found between indexes[%d] = [%d]\n", min, max);
+	for (; min <= max; min++)
+	{
+		printf("Value checked array[%d] = [%d]\n", min, *(array +
+								  min));
+		if (value == *(array + min))
 			return (min);
-		}
 	}
 
 	return (-1);
