0a57e15cb2c8ff6ff7828f3fe1d69ee7ba375d95
NatnaelM77 <natnaelmehari77@gmail.com>
2022-05-09 21:50:39 +0300

    Exponential search

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -13,5 +13,64 @@
 
 int exponential_search(int *array, size_t size, int value)
 {
-	return (0);
+	int step = 1;
+	int min, mid, low, high;
+
+	if (!array || size == 0)
+		return (-1);
+
+	if (array[0] == value)
+		return (0);
+
+	while (array[step] <= value && step < size)
+	{
+		printf("Value checked array[%d] = [%d]\n", step, array[step]);
+		min = step;
+		step *= 2;
+		if (step > size - 1)
+		{
+			step -= 1;
+			break;
+		}
+	}
+
+	printf("Value found between indexes [%d] and [%d]\n", min, step);
+
+	low = min;
+	high = step;
+
+	while (low <= high)
+	{
+		mid = low + (high - low) / 2;
+		print_array(array, low, high);
+
+		if (value == array[mid])
+			return (mid);
+
+		if (value > array[mid])
+			low = mid + 1;
+		else
+			high = mid - 1;
+	}
+
+	return (-1);
+}
+
+/**
+ * print_array - print array from min to max
+ *
+ * @array: array of int
+ * @min: min element of array
+ * @max: max element of array
+ *
+ */
+
+void print_array(int *array, int min, int max)
+{
+	printf("Searching in array: ");
+	for (; min < max; min++)
+	{
+		printf("%d, ", array[min]);
+	}
+	printf("%d\n", array[min]);
 }
--- 0x1E-search_algorithms/main.c
+++ 0x1E-search_algorithms/main.c
@@ -9,20 +9,20 @@
 int main(void) {
 
 	int a1[] = {
-		0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-	};
+        0, 1, 2, 3, 4, 7, 12, 15, 18, 19, 23, 54, 61, 62, 76, 99
+    	};
 	size_t size = sizeof(a1) / sizeof(a1[0]);
 
-	printf("Found %d at index: %d\n\n", 6, jump_search(a1, size, 6));
-	printf("Found %d at index: %d\n\n", 1, jump_search(a1, size, 1));
-	printf("Found %d at index: %d\n", 999, jump_search(a1, size, 999));
+	printf("Found %d at index: %d\n\n", 6, exponential_search(a1, size, 62));
+	printf("Found %d at index: %d\n\n", 1, exponential_search(a1, size, 3));
+	printf("Found %d at index: %d\n", 999, exponential_search(a1, size, 999));
 
 	int a2[15] = {
 		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 23, 47, 89, 160
 	};
-	printf("Found at index: %d\n", jump_search(a2, 15, 11));
-	printf("Found at index: %d\n", jump_search(a2, 15, 9));
-	printf("Found at index: %d\n", jump_search(a2, 15, 123456789));
+	printf("Found %d at index: %d\n", 11, exponential_search(a2, 15, 11));
+	printf("Found %d at index: %d\n", 9, exponential_search(a2, 15, 9));
+	printf("Found %d at index: %d\n", 123456789, exponential_search(a2, 15, 123456789));
 
 	return (0);
 }
